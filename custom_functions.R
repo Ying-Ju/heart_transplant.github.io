db5c2f485d9e2f4472a3d976491e444c17a6e993
Ying-Ju Chen <tessa0111@gmail.com>
2020-08-03 23:34:22 -0400

    update some files

--- custom_functions.R
+++ custom_functions.R
@@ -57,7 +57,7 @@ DOE_function <- function(ii, design_scenario, all_data, y, all_ID, holdout_ID_in
   conflict_prefer("select", "dplyr")
   
   scenario <- design_scenario[ii,]
-  set.seed <- seed
+  set.seed(seed)
   
   # obtain the necessary information regarding the scenario
   fold <- scenario$fold
@@ -325,7 +325,7 @@ select_variables <- function(ii, all_data, y, method, ID_all, ID_index, seed=201
   if(require(pacman)==FALSE) install.packages("pacman")
   pacman::p_load(Biocomb, glmnet, party, ranger)
   
-  set.seed <- seed	
+  set.seed(seed)	
   input_data <- as.data.frame(all_data[[ii]])
   all_types <- unlist(lapply(input_data, class))
   char.index <- which(all_types=="factor")
@@ -379,4 +379,261 @@ select_variables <- function(ii, all_data, y, method, ID_all, ID_index, seed=201
 }
 
 
+train_iso<-function(iii, t_data,h_data,features,folds=5,resampl_meth="up",alg_used,seed0=2019){
+  
+  # train_iso(): a function that train a desired machine learning algorithm over multiple time stamps
+  # iii: index used to navigate to different time stamps
+  # t_data: the train datasets
+  # t_data: the holdout datasets
+  # features: independent features
+  # folds: number of hods for cross validation
+  # resampl_meth: the resampling algorithm for balancing the dependent variable
+  # alg_used: the selected machine learning algorithm
+  # seed: the random seed
+  
+  # since this function is usually used with a parallel algorithm, we include all 
+  # necessary packages
+  
+  gc()
+  if(!"pacman" %in% rownames(installed.packages())){
+    install.packages(pkgs = "pacman",repos = "http://cran.us.r-project.org")
+  }
+  # p_load is equivalent to combining both install.packages() and library()
+  pacman::p_load(caret,AUC,MASS,ROSE,DMwR,snow,ranger,parallel,xgboost,gbm,naivebayes,e1071,kernlab,pls,parallel)
+  
+  set.seed(seed0)
+  fold_no<-folds
+  alg_fact<-alg_used
+  resample_fact<-resampl_meth
+  coef<-"NO"
+  var_imp<-"NO"
+  
+  df <- t_data[[iii]]
+  hold_out_ <- h_data[[iii]]
+  traindata <- df[c(as.character(features[[paste0("year",iii-1)]][,]),paste0("year",iii-1) )]
+  hold_out<-hold_out_[c(as.character(features[[paste0("year",iii-1)]][,]),paste0("year",iii-1),"ID" )]
+  TARGET0<-paste0("year",iii-1)
+  
+  traindata[[TARGET0]] <- as.factor(traindata[[TARGET0]])
+  levels(traindata[[TARGET0]]) <- list(Survival="Survival", Death="Death")
+  hold_out[[TARGET0]] <- as.factor(hold_out[[TARGET0]])
+  levels(hold_out[[TARGET0]]) <- list(Survival="Survival", Death="Death")
+  
+  formul<-as.formula(paste0(as.character(TARGET0),"~."))
+  
+  run.code<-"YES"
+  sampling.method<-resample_fact
+  if(resample_fact=="none"){sampling.method<-NULL}
+  # Reference for geometric mean
+  # Kim, Myoung-Jong, Dae-Ki Kang, and Hong Bae Kim. "Geometric mean based boosting 
+  # algorithm with over-sampling to resolve data imbalance problem for bankruptcy prediction." 
+  # Expert Systems with Applications 42.3 (2015): 1074-1082.
+  
+  gmeanfunction <- function(data, lev = NULL, model = NULL) {
+    sub_sens<-caret::sensitivity(data$pred,data$obs)
+    sub_spec<-caret::specificity(data$pred,data$obs)
+    c(gmean = sqrt(sub_sens*sub_spec))
+  }
+  
+  
+  
+  # I used 5 fold cross validation 
+  control_setting <- caret::trainControl(method = "cv", number=fold_no, sampling=sampling.method , 
+                                         #summaryFunction = twoClassSummary, 
+                                         # the next one is for saving the predictions
+                                         savePredictions = "all",
+                                         search="random", classProbs = TRUE, selectionFunction="best"
+                                         ,summaryFunction = gmeanfunction)
+  
+  
+  if (alg_fact%in% c("glm", "nnet", "svmRadial")){
+    
+    if((class(try( result_model <- train(formul, data=traindata, method=alg_fact, family="binomial",
+                                         trControl = control_setting, metric="gmean"),silent = TRUE))=="try-error")[1]){run.code<-"NO"}
+    
+    
+    
+  }else if (alg_fact%in%c("rf", "gbm", "earth", "rpart", "xgbTree", "naive_bayes","xgbDART" ,"ranger","glmnet")){
+    
+    if((class(try( result_model <- train(formul,  data=traindata, method=alg_fact,
+                                         trControl = control_setting, tuneLength=10, metric="gmean"),silent = TRUE))=="try-error")[1]){ run.code<-"NO"}
+    
+    
+  }else if(alg_fact=="lda"){
+    if((class(try(  result_model <- train(formul, data=traindata, method=alg_fact, preProcess="pca", preProcOptions = list(method="BoxCox"),
+                                          trControl = control_setting, metric="gmean"),silent = TRUE))=="try-error")[1]){run.code<-"NO"}
+    
+    
+  }else if(alg_fact=="treebag"){
+    if((class(try(  result_model <- train(formul, data=traindata, method=alg_fact, family="binomial",
+                                          trControl = control_setting, tuneLength=10, metric="gmean"),silent = TRUE))=="try-error")[1]){run.code<-"NO"}  
+    
+  }
+  
+  if(run.code=="YES"){
+    
+    coef<-as.data.frame(summary(result_model)$coefficients)
+    coef$vars<-rownames(coef)
+    coef$vars<-as.character(sapply(coef$vars,function(x) gsub("1","",x)))
+    coef$vars[which(coef$vars=="(Intercept)")]<-"INTERCEPT"
+    
+    coef$vars[match("DAYS_STAT1",rownames(coef))]<-"DAYS_STAT1"
+    coef$vars[match("DAYS_STAT1A",rownames(coef))]<-"DAYS_STAT1A"
+    coef$vars[match("DAYS_STAT1B",rownames(coef))]<-"DAYS_STAT1B"
+    
+    if(alg_fact=="glm"){
+      var_imp<-as.data.frame(caret::varImp(result_model)$importance)
+      var_imp$vars<-rownames(var_imp)
+      var_imp$vars<-as.character(sapply(var_imp$vars,function(x) gsub("1","",x)))  
+      var_imp$vars[match("DAYS_STAT1",rownames(var_imp))]<-"DAYS_STAT1"
+      var_imp$vars[match("DAYS_STAT1A",rownames(var_imp))]<-"DAYS_STAT1A"
+      var_imp$vars[match("DAYS_STAT1B",rownames(var_imp))]<-"DAYS_STAT1B"
+    }
+    
+    
+    resul_raw <- as.data.frame(matrix(NA, ncol = 4, nrow = nrow(hold_out)))
+    colnames(resul_raw) <- c("TARGET_raw", alg_fact, "Probability","ID")
+    resul_raw$TARGET_raw <- hold_out[as.character(TARGET0)]
+    resul_raw$ID<-hold_out$ID
+    
+    #train_raw <- as.data.frame(matrix(NA, ncol = 3, nrow = nrow(traindata)))
+    #colnames(train_raw) <- c("TARGET", alg_fact, "Probability")
+    #train_raw$TARGET <- traindata[as.character(TARGET0)]
+    
+    resul_pred_perf<-as.data.frame(matrix(NA, ncol = 1, nrow = 5))
+    colnames(resul_pred_perf)<-c(alg_fact)
+    rownames(resul_pred_perf)<-c("auc","sen","spec","accu","gmean")
+    
+    resul_pred_perf_folds<-as.data.frame(matrix(NA, ncol = fold_no, nrow = 5))
+    colnames(resul_pred_perf_folds)<-paste0("fold",1:fold_no)
+    rownames(resul_pred_perf_folds)<-c("auc","sen","spec","accu","gmean")
+    
+    fold_res<-list()
+    
+    for (j in 1:fold_no) {
+      fold_res[[j]]<-result_model$pred[which(result_model$pred$Resample==paste0("Fold",j)),]
+      resul_pred_perf_folds["auc",j]<-AUC::auc(roc(fold_res[[j]]$Survival ,fold_res[[j]]$obs))
+      resul_pred_perf_folds["sen",j]<-caret::sensitivity(fold_res[[j]]$pred,fold_res[[j]]$obs)
+      resul_pred_perf_folds["spec",j]<-caret::specificity(fold_res[[j]]$pred,fold_res[[j]]$obs)
+      resul_pred_perf_folds["accu",j]<-(as.data.frame(confusionMatrix(fold_res[[j]]$pred, fold_res[[j]]$obs)$overall))[1,]
+      resul_pred_perf_folds["gmean",j]<-sqrt(resul_pred_perf_folds["sen",j]*resul_pred_perf_folds["spec",j])
+    }
+    fold_res[["performance"]]<-resul_pred_perf_folds
+    
+    
+    resul_raw[alg_fact] <- predict(result_model, newdata=hold_out, type="raw")
+    resul_raw$Probability <- predict(result_model, newdata=hold_out, type="prob")[,1]
+    resul_pred_perf[1,1] <- AUC::auc(roc(resul_raw$Probability,as.factor(as.data.frame(hold_out[,TARGET0])[,1])))
+    resul_pred_perf[2,1] <- caret::sensitivity(resul_raw[,alg_fact],as.factor(as.data.frame(hold_out[,TARGET0])[,1]))
+    resul_pred_perf[3,1] <- caret::specificity(resul_raw[,alg_fact],as.factor(as.data.frame(hold_out[,TARGET0])[,1]))
+    resul_pred_perf[4,1] <- (as.data.frame(confusionMatrix(resul_raw[,alg_fact], as.factor(as.data.frame(hold_out[,TARGET0])[,1]))$overall))[1,]
+    resul_pred_perf[5,1] <- sqrt(resul_pred_perf[2,1]*resul_pred_perf[3,1])
+    
+    gmean_overal<-as.data.frame(result_model$results)
+    gmean_folds<-as.data.frame(result_model$resample)
+    equat<-as.data.frame(result_model$finalModel$coefficients)
+    names(equat)<-"coef"
+    equat$vars<-rownames(equat)
+    equat$vars[which(equat$vars==c("(Intercept)"))]<-c("INTERCEPT")
+    
+    max_gmean<-max(result_model$resample$gmean)
+    fold_pick<-result_model$resample[which(result_model$resample$gmean==max_gmean),c("Resample")]
+    foldbest<-result_model$pred[which(result_model$pred$Resample==fold_pick),c("obs","pred","Survival")]
+    names(foldbest)<-c(paste0("year",iii-1),"log","Probability")
+    
+    
+    dir.create(paste0(getwd(),"/data_pile"))
+    # the next line remove everything except for the mentioned data franes
+    rm(list=setdiff(ls(), c("resul_pred_perf","resul_raw","alg_fact","iii","coef","var_imp",
+                            "gmean_overal","gmean_folds","equat","foldbest","fold_res")))
+    
+    saveRDS(list(Performance=resul_pred_perf, Predicted=resul_raw,coef=coef,var_imp=var_imp,gmean_overal=gmean_overal,gmean_folds=gmean_folds,
+                 equat=equat,foldbest=foldbest ),
+            paste0(getwd(),"/data_pile/",alg_fact,"-year-",iii-1,".rds"))
+    
+    return(list(Performance=resul_pred_perf, Predicted=resul_raw,coef=coef,var_imp=var_imp,gmean_overal=gmean_overal,gmean_folds=gmean_folds,
+                equat=equat,foldbest=foldbest,folds_res=fold_res))
+  }else{
+    dir.create(paste0(getwd(),"/data_pile"))
+    rm(list=setdiff(ls(), c("experiment.summary","alg_fact","iii","coef","var_imp")))
+    save.image(paste0(getwd(),"/data_pile/NOT-",alg_fact,"-",iii,".RData"))
+    saveRDS(list(Performance="NOT", Predicted="NOT",coef=coef,var_imp=var_imp,gmean_overal="NOT",gmean_folds="NOT",
+                 equat="NOT",foldbest="NOT"),
+            paste0(getwd(),"/data_pile/NOT-",alg_fact,"-year-",iii-1,".rds"))
+    return(list(Performance="NOT", Predicted="NOT",coef=coef,var_imp=var_imp,gmean_overal="NOT",gmean_folds="NOT",
+                equat="NOT",foldbest="NOT",folds_res="NO"))
+  }
+}
 
+
+select_variables_iso<-function(ii,data,method,exclud="ID",folds=5,alpha=1,seed=110){
+  # select_variables_iso(): a function for the variable selection for mutiple time stamps
+  # ii: index used to navigate to different data sets 
+  # data: a list of all datasets
+  # exclud: the variable(s) that should be excluded from the dataserts
+  # method: Fast Feature Correlation Based Selection (FFS), Least absolute shrinkage and selection operator (LASSO), Random Forest (RF)
+  # seed: the random seed
+  
+  set.seed(seed)
+  dependent<-paste0("year",ii-1)
+  
+  excluds<-c(exclud,dependent)
+  dataset<-as.data.frame(data[[ii]])
+  
+  X<-dataset[!names(dataset)%in%excluds]
+  input_data<-X
+  input_data[,dependent] <- as.factor(dataset[,dependent])
+  
+  if (method=="FFS"){
+    X$y <- as.factor(dataset[,dependent])
+    disc <- "MDL"
+    threshold <- 0.001
+    attrs.nominal <- numeric()
+    result_temp <- Biocomb::select.fast.filter(X, disc.method=disc, threshold=threshold, attrs.nominal=attrs.nominal)
+    Imp_vars <- as.data.frame(as.character(result_temp$Biomarker))
+    colnames(Imp_vars) <- c("variables")
+    Imp_vars <- list(Imp_vars)
+    names(Imp_vars) <- paste0("year", ii-1)
+    
+  }else if (method=="LASSO"){
+    '%ni%' <- Negate('%in%')
+    X <- data.matrix(apply(X, 2, as.numeric))
+    glmnet1 <- glmnet::cv.glmnet(x=X,y=as.factor(dataset[,dependent]),type.measure='auc',nfolds=folds,alpha=alpha, family="binomial")
+    co <- coef(glmnet1,s = "lambda.1se")
+    co <- as.matrix(co[-1,])
+    Imp_vars <- row.names(co)[which((co[,1]!=0))]
+    Imp_vars <-  as.data.frame(Imp_vars[Imp_vars %ni% '(Intercept)'])
+    colnames(Imp_vars) <- c("variables")
+    Imp_vars <- list(Imp_vars)
+    names(Imp_vars) <- paste0("year", ii-1)}else if (method=="RF"){
+      Success <- 0
+      iteration <- 0
+      while (Success==0 & iteration <=10){
+        rf <- ranger(formula(paste0(dependent, "~.")), data=input_data, num.trees = 500, importance = "permutation",
+                     scale.permutation.importance = FALSE,  mtry = floor(0.2 * ncol(X)),
+                     min.node.size = floor(0.1 * nrow(X)),
+                     num.threads = 1,
+                     write.forest = TRUE)
+        if (min(importance(rf))<0){
+          Imp_values <- importance(rf)/abs(min(importance(rf)))
+          Imp_vars <- names(which(Imp_values>1))
+          Success <- 1
+        }else{
+          iteration <- iteration + 1
+        }
+      }
+      if (iteration>10){
+        print("At least 10 runs with no negative importance score!")
+        Imp_vars <- NA
+      }
+      
+      Imp_vars <-  as.data.frame(Imp_vars)
+      colnames(Imp_vars) <- c("variables")
+      Imp_vars <- list(Imp_vars)
+      names(Imp_vars) <- paste0("year", ii-1)
+      
+    }
+  
+  
+  return(Imp_vars)
+}
